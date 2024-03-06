76e91f8fcc76495b5689cb4f9faac2249d1090d4
aradhana1807 <aradhana.zero@gmail.com>
2024-03-06 11:48:40 +0530

    added Header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,11 @@
-import { Inter } from "next/font/google";
+import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
 
+import Header from "@/components/Header";
+
 const inter = Inter({ subsets: ["latin"] });
+
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Movie DB",
@@ -11,7 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={montserrat.className}>
+        <Header />
+        {children}</body>
     </html>
   );
 }
