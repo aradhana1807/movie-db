4f262e8b718864172d245e6c1752222cadbca618
aradhana1807 <aradhana.zero@gmail.com>
2024-03-06 14:42:52 +0530

    added error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import NavbarItem from './NavbarItem';
 
 export default function Navbar() {
     return (
-        <div className='flex dark:bg-gray-700 bg-slate-100 p-4 lg:text-lg justify-center gap-6'>
+        <div className='flex dark:bg-gray-700 bg-slate-200 p-4 lg:text-lg justify-center gap-6'>
             <NavbarItem title='Trending' param='fetchTrending' />
             <NavbarItem title='Top Rated' param='fetchTopRated' />
         </div>
