b52b52ecf31d1e4ed12e29eac5cb70d69d667c33
aradhana1807 <aradhana.zero@gmail.com>
2024-03-06 14:34:51 +0530

    fetch data from tmdb

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,18 +1,22 @@
-"use client"
-import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+'use client';
+
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
 
 export default function NavbarItem({ title, param }) {
     const searchParams = useSearchParams();
-    const genre = searchParams.get("genre");
+    const genre = searchParams.get('genre');
     return (
         <div>
             <Link
                 className={`hover:text-emerald-600 font-semibold ${genre === param
-                    ? 'underline underline-offset-8 decoration-4 decoration-emerald-700 rounded-lg'
-                    : ''
+                        ? 'underline underline-offset-8 decoration-4 decoration-ember-500 rounded-lg'
+                        : ''
                     }`}
-                href={`/?genre=${param}`}>{title}</Link>
-        </div >
-    )
+                href={`/?genre=${param}`}
+            >
+                {title}
+            </Link>
+        </div>
+    );
 }
